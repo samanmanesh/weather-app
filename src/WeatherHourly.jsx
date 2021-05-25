0eb31fc954fd12d59fde8259aeb0c0d1a3ec344f
Saman Manesh <samanmanesh7@gmail.com>
2021-05-25 17:23:03 -0400

    removed logs

--- src/WeatherHourly.jsx
+++ src/WeatherHourly.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import dayjs from 'dayjs';
 
 export default function WeatherHourly({weatherDailyData}) {
 
     const hourly = weatherDailyData.hourly;
-
-    /**
-     *  clouds: 100
-        dew_point: 15.18
-        dt: 1621976400
-        feels_like: 29.25
-        humidity: 42
-        pop: 0.05
-        pressure: 1012
-        temp: 29.42
-        uvi: 3.36
-     */
-    console.log(hourly)
 
     const getTime = interval => {
         if (interval === 0) return 'Now'
