f6f01df8acd28dfcfa9fb0fcf1a2f6487c74c1fa
Saman Manesh <samanmanesh7@gmail.com>
2021-05-25 12:43:57 -0400

    adding some styles

--- src/App.js
+++ src/App.js
@@ -7,50 +7,61 @@ const URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "6e4fed69198e988a933dfe45e62b901e";
 
 function App() {
-  
   const [weatherData, setWeatherData] = useState();
   // const [query, setQuery] = useState("");
-  const ref = useRef()
- 
+  const ref = useRef();
+
   const getWeather = (queries) => {
-    console.log('getting weather...')
-    axios.get(URL, {
+    // console.log('getting weather...')
+    axios
+      .get(URL, {
         params: {
           q: queries,
           units: "metric",
           APPID: API_KEY,
         },
-      }).then((res) => {
+      })
+      .then((res) => {
+        console.log(res);
         // console.log('res >>', res);
         // const { data } = res;
         // console.log('destructured data >>', data)
-        setWeatherData(res.data)});
+        // console.log(res.data);
+        setWeatherData(res.data);
+      });
   };
-  
-  const search = e => {
-    if (e.key === 'Enter') 
-      getWeather(ref.current.value);
-  }
+
+  useEffect(() => {
+    getWeather("Toronto");
+  }, [])
+  const search = (e) => {
+    if (e.key === "Enter") getWeather(ref.current.value);
+  };
 
   // useEffect(() => {
   //   navigator.geolocation.getCurrentPosition(e => console.log('got', e))
   // }, [])
-  
+
   return (
     <div className="App">
-      {weatherData && <WeatherData weatherData={weatherData} />}
-      <input
-        type="text"
-        className="search"
-        placeholder="Search..."
-        // value={query}
-        ref={ref}
-        // onChange={e => setQuery(e.target.value)}
-        onKeyPress={search}
-      />
+      <div className="today-section">
+        {weatherData && <WeatherData weatherData={weatherData} />}
+        <input
+          type="text"
+          className="search"
+          placeholder="Search..."
+          // value={query}
+          ref={ref}
+          // onChange={e => setQuery(e.target.value)}
+          onKeyPress={search}
+        />
 
-      {/* <button onClick={() => getWeather("Toronto")}>Toronto</button>
+        {/* <button onClick={() => getWeather("Toronto")}>Toronto</button>
       <button onClick={() => getWeather("London")}>London</button> */}
+      </div>
+      <div className="today-time-section">time section</div>
+      <div className="weekdays-section">weekdays section</div>
+      <div className="todays-details">todays details</div>
     </div>
   );
 }
--- src/WeatherData.jsx
+++ src/WeatherData.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 export default function WeatherData({weatherData}) {
+   
+    // /// Capitalized the first letter of each words but I used css instead
+    // const convertingUpperCase = () =>{
+    //     const str = weatherData.weather[0].description
+    //     function capitalize(str) {
+    //         return str.charAt(0).toUpperCase() + str.slice(1);
+    //       }
+          
+    //       const caps = str.split(' ').map(capitalize).join(' ');
+    //   return caps;    
+    // }
     return (
         <div>
-            <h2>{Math.round(weatherData.main.temp)}ºC</h2>
-            <h3>{weatherData.weather[0].description}</h3>
+            <div className="city-name">{weatherData.name}</div>
+            {/* <div className="description">{convertingUpperCase()}</div> */}
+            <div className="description">{weatherData.weather[0].description}</div>
+            <div className="temp">{Math.round(weatherData.main.temp)}º</div>
+            <div className="temp-high-low">H:{Math.round(weatherData.main.temp_max)}° &nbsp; L:{Math.round(weatherData.main.temp_min)}°</div>
         </div>
     )
 }
